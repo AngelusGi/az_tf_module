293784de5285e56b0bcc3271c224e25508ce58ce
Angelo Gino Varrati <17298854+AngelusGi@users.noreply.github.com>
2025-09-15 17:15:28 +0200

    fix identity

--- aks/main.tf
+++ aks/main.tf
@@ -45,7 +45,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 }
 
 resource "azurerm_role_assignment" "registry_pull" {
-  principal_id                     = azurerm_kubernetes_cluster.k8s.kubelet_identity.object_id
+  principal_id                     = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
   scope                            = var.docker_registry_id
   skip_service_principal_aad_check = false
