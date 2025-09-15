af6194eb7e0fd67630a9bcac47194c2dd6ce1268
Angelo Gino Varrati <17298854+AngelusGi@users.noreply.github.com>
2025-09-15 17:05:38 +0200

    wip

--- aks/output.tf
+++ aks/output.tf
@@ -13,11 +13,6 @@ output "network_profile" {
   sensitive = false
 }
 
-output "subnet" {
-  value = azurerm_subnet.aks.address_prefix
-  sensitive = false
-}
-
 output "kube_config" {
   description = "Kube config for AKS cluster"
   value = azurerm_kubernetes_cluster.k8s.kube_config_raw
